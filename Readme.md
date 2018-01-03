d9eab221efe2c36b404030059a2b526dd56eab29
Naz Junaid <nazmul187@gmail.com>
2018-01-03 18:15:15 +0000

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,45 @@
+﻿## About
+Index funds allow you to track a performance of a market rather than holding individual assets e.g S&P500, FTSE100 etc. these tend to consistently [outperform managed funds](http://fortune.com/2017/04/13/stock-indexes-beat-mutual-funds/).
+
+> **"Don’t look for the needle in the haystack. Just buy the haystack!"**
+> 
+> John C. Bogle, founder of The Vanguard Group and inventor of the index fund.
+
+There are now thousands of cryptocurrency projects to pick from and it can be difficult and time consuming to find the good projects. Holding only Bitcoin and/or Ethereum limits your exposure to the money coming into the market.
+
+With an index fund you don't worry about deciding what, when and how much to invest in your coins allowing you to not make any emotional based decisions.
+
+This tool helps save time figuring out how much of an asset to hold and when it comes to re-balancing what to buy and sell. It utilizes the [Capitalization-Weighted Index](https://www.investopedia.com/terms/c/capitalizationweightedindex.asp) and the [Capped Index](https://www.investopedia.com/terms/c/capped-index.asp) and has a number of hyperparameters you can tweak.
+
+There are tokens such as the [Crypto20 Token index fund](https://crypto20.com/en/) that do this for you autonomously, MyCryptoIndexFund is for people that want to hold their own coins rather relying on 3rd parties.
+
+
+## How to use
+NOTE: I'm not a financial adviser and these ideas do not represent and financial advice. Investing in Crypto is risky and the market can change anytime.
+
+You can use this tool directly on [MyCryptoIndexFund.com](https://www.mycryptoindexfund.com/) or download the open source code from Github and run this locally.
+
+SImply enter in how much to invest and select the coins your interested in and the tool essentially calculates the ideal weight per coin and for convenience tells you how much quantity of that coins that equates to a long with a few other details.
+
+Periodically (week/monthly) you'll want to re-balance your portfolio. Once you enter in your current quantity holdings the re-balancing feature will tell you how much you need to buy/sell to get your holdings back to the correct weightings.
+
+## Some strategies
+
+There are a few different strategies you can try, here are some ideas.
+
+1. Just buy the Top 20 with a max weight of 10% per coin similar to Crypto20. Probably the safest choice and so far [has outperformed just buying bitcoin](https://crypto20.com/en/portal/performance/).
+
+2. Buy the top 5-10 coins without max weightings. This is good if you prefer to have more Bitcoin and want less coins to manage.
+
+3. Only use it for Alt-coins with/without the bitcoin forks.
+
+
+Please play around with MyCryptoIndexFund.com, I'd be interested to hear how your using it feel free the raise any ideas or issues you have with the tool.
+
+
+## Donate
+If it works out for you and you make a ton of money I'll happily take any tips :D
+
+- BTC (Segwit): 39cejK3pXpm4fqpDEwLXUzmViWGsZ8cZxm
+- BCH: 1MMC5a7qd5Vi1otY1b9SkmoBABUkquCeso
+- LTC: MKwyJGwkaQPqbaTP3dYwhmxcjKBQJ4sFGz
---
+++ js/main.js
@@ -0,0 +1,282 @@
+﻿(function(Vue, $, localStorage, numeral) {
+
+    var db = {
+        get mode() {
+            return localStorage.getItem("mode") == null ? "default" : localStorage.getItem("mode");
+        },
+        set mode(value) {
+            localStorage.setItem("mode", value);
+        },
+        get maxPercentage() {
+            return localStorage.getItem("maxPercentage") == null ? 10 : localStorage.getItem("maxPercentage");
+        },
+        set maxPercentage(value) {
+            localStorage.setItem("maxPercentage", value);
+        },
+        get investmentAmount() {
+            return parseFloat(localStorage.getItem("investmentAmount") == null ? 1500 : localStorage.getItem("investmentAmount"));
+        },
+        set investmentAmount(value) {
+            localStorage.setItem("investmentAmount", value);
+        },
+        get investmentType() {
+            return localStorage.getItem("investmentType") == null ? "USD" : localStorage.getItem("investmentType");
+        },
+        set investmentType(value) {
+            localStorage.setItem("investmentType", value);
+        },
+        get selectedOnly() {
+            return localStorage.getItem("selectedOnly") == null || localStorage.getItem("selectedOnly") === "true";
+        },
+        set selectedOnly(value) {
+            localStorage.setItem("selectedOnly", value);
+        },
+        get selectedCoins() {
+            return localStorage.getItem("selectedCoins") == null ? null : JSON.parse(localStorage.getItem("selectedCoins"));
+        },
+        set selectedCoins(value) {
+            localStorage.setItem("selectedCoins", JSON.stringify(value));
+        }
+    };
+
+    function Coin(data) {
+        var initialData = data;
+
+        this.init = function(data, isReset) {
+            this.name = data.name;
+            this.symbol = data.symbol;
+            this.rank = data.rank;
+            this.price = data.price;
+            this.priceBtc = data.priceBtc;
+            this.isSelected = data.isSelected;
+            this.marketCap = data.marketCap;
+            this.holdingQuantity = isReset ? 0 : data.holdingQuantity;
+            this.isSelected = isReset ? false : data.isSelected;
+            this.weight = 0;
+            this.currentWeight = 0;
+            this.weightMoney = 0;
+            this.quantity = 0;
+            this.buyQuantity = 0;
+            this.sellQuantity = 0;
+        };
+
+        this.reset = function() {
+            this.init(initialData, true);
+        };
+
+        this.init(initialData, false);
+    }
+
+    var scamCoins = ["BCC"];
+
+    var vm = new Vue({
+        el: "#app",
+        data: {
+            mode: db.mode,
+            maxPercentage: db.maxPercentage,
+            investmentAmount: db.investmentAmount,
+            investmentType: db.investmentType,
+            selectedOnly: db.selectedOnly,
+            coins: [],
+            numeral: numeral,
+            importMode: "Import",
+            importDataText: "",
+            exportDataText: ""
+        },
+        methods: {
+            saveChanges: function(event) {
+
+                var selectedCoins = [];
+
+                for (var i = 0; i < this.selectedCoins.length; i++) {
+                    var coin = this.selectedCoins[i];
+
+                    if (coin.isSelected) {
+                        selectedCoins.push({
+                            symbol: coin.symbol,
+                            holdingQuantity: coin.holdingQuantity
+                        });
+                    }
+                }
+
+                db.mode = this.mode;
+                db.maxPercentage = this.maxPercentage;
+                db.investmentAmount = this.investmentAmount;
+                db.investmentType = this.investmentType;
+                db.selectedOnly = this.selectedOnly;
+                db.selectedCoins = selectedCoins;
+            },
+            reset: function() {
+                localStorage.clear();
+                window.location.reload();
+            },
+            launchImporter: function() {
+                $("#dataImporter").modal();
+
+                this.exportDataText = JSON.stringify(JSON.stringify(localStorage));
+            },
+            importData: function() {
+                try {
+                    var data = JSON.parse(JSON.parse(this.importDataText));
+
+                    localStorage.clear();
+
+                    Object.keys(data).forEach(function(k) {
+                        localStorage.setItem(k, data[k]);
+                    });
+
+                    window.location.reload();
+                } catch (e) {
+                    alert("Import failed, please make sure you copy/pasted the full text");
+                    throw e;
+                }
+            }
+        },
+        computed: {
+            isRebalancing: function() {
+                return this.mode === "rebalancing";
+            },
+            isBtc: function() {
+                return this.investmentType === "BTC";
+            },
+            currencySymbol: function() {
+                return this.isBtc ? "Ƀ" : "$";
+            },
+            selectedCoins: function() {
+                var totalCap = 0;
+
+                var selectedCoins = [];
+                for (var j = 0; j < this.coins.length; j++) {
+                    var coin = this.coins[j];
+
+                    if (coin.isSelected === true) {
+                        totalCap += coin.marketCap;
+                        selectedCoins.push(coin);
+                    } else {
+                        coin.reset();
+                    }
+                }
+
+                var totalCapRemaining = totalCap;
+                var percentageRemaining = 1;
+                var maxPercentageFraction = parseFloat(this.maxPercentage / 100);
+
+                for (var i = 0; i < selectedCoins.length; i++) {
+                    var selectedCoin = selectedCoins[i];
+
+                    selectedCoin.weight = Math.min(maxPercentageFraction, percentageRemaining * selectedCoin.marketCap / totalCapRemaining);
+                    selectedCoin.weightMoney = selectedCoin.weight * this.investmentAmount;
+                    selectedCoin.quantity = selectedCoin.weightMoney / (this.isBtc ? selectedCoin.priceBtc : selectedCoin.price);
+                    selectedCoin.currentWeight = (selectedCoin.holdingQuantity * (this.isBtc ? selectedCoin.priceBtc : selectedCoin.price)) / this.investmentAmount;
+
+                    var quantityDifference = selectedCoin.quantity - selectedCoin.holdingQuantity;
+
+                    if (quantityDifference < 0) {
+                        selectedCoin.sellQuantity = quantityDifference;
+                        selectedCoin.buyQuantity = 0;
+                    } else {
+                        selectedCoin.buyQuantity = quantityDifference;
+                        selectedCoin.sellQuantity = 0;
+                    }
+
+                    percentageRemaining -= selectedCoin.weight;
+                    totalCapRemaining -= selectedCoin.marketCap;
+                }
+
+                return selectedCoins;
+            },
+            coinsToShow: function() {
+                if (this.selectedOnly) {
+                    return this.selectedCoins;
+                } else {
+                    return this.coins;
+                }
+            },
+            totalPortfolioWorth: function() {
+                var total = 0;
+                for (var i = 0; i < this.selectedCoins.length; i++) {
+                    var coin = this.selectedCoins[i];
+
+                    total += (this.isBtc ? coin.priceBtc : coin.price) * coin.holdingQuantity;
+                }
+
+                return parseFloat(total).toFixed(this.isBtc ? 8 : 2);
+            },
+            sellTotalPrices: function() {
+                var isBtc = this.isBtc;
+
+                return this.selectedCoins.map(function(coin) {
+                    var price = coin.sellQuantity * (isBtc ? coin.priceBtc : coin.price);
+                    return (isBtc && price < -0.001) || price < -20 ? price : null;
+                });
+            },
+            buyTotalPrices: function() {
+                var isBtc = this.isBtc;
+
+                return this.selectedCoins.map(function(coin) {
+                    var price = coin.buyQuantity * (isBtc ? coin.priceBtc : coin.price);
+
+                    return (isBtc && price > 0.001) || price > 20 ? price : null;
+                });
+            }
+        },
+        created: function() {
+            $.get({
+                url: "https://api.coinmarketcap.com/v1/ticker/?limit=100",
+                cache: false
+            }).then(function (response) {
+
+                var selectedCoins = db.selectedCoins == null ? [] : db.selectedCoins;
+                if (!selectedCoins.length) {
+                    for (var i = 0; i < response.length; i++) {
+                        for (var j = 0; j < scamCoins.length; j++) {
+                            var isScam = scamCoins[j] === response[i].symbol;
+                            if (!isScam) {
+                                selectedCoins.push({ symbol: response[i].symbol });
+                            }
+                        }
+                        if (selectedCoins.length >= 20) {
+                            break;
+                        }
+                    }
+                }
+
+                var coins = response.map(function(coin) {
+
+                    var match = null;
+                    for (var k = 0; k < selectedCoins.length; k++) {
+                        if (selectedCoins[k].symbol === coin.symbol) {
+                            match = selectedCoins[k];
+                        }
+                    }
+
+                    var isSelected = match != null;
+                    var holdingQuantity = match == null || !match.holdingQuantity ? 0 : match.holdingQuantity;
+
+                    var newCoin = new Coin({
+                        name: coin.name,
+                        symbol: coin.symbol,
+                        rank: coin.rank,
+                        price: parseFloat(coin.price_usd),
+                        priceBtc: parseFloat(coin.price_btc),
+                        marketCap: parseFloat(coin.market_cap_usd),
+                        isSelected: isSelected,
+                        holdingQuantity: holdingQuantity
+                    });
+
+                    return newCoin;
+                });
+
+                vm.coins = coins;
+            }).fail(function() {
+                alert("Error getting data from coinmarketcap.com");
+                console.log(response.body);
+            });
+        }
+    });
+
+    vm.$watch("$data", function() {
+        vm.saveChanges();
+    }, { deep: true });
+
+})(window.Vue, window.jQuery, window.localStorage, window.numeral);
